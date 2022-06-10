febf7c88afdd4ddba877fffa0874348c90999cf0
V. David Zvenyach <dave@zvenyach.com>
2022-06-09 20:52:23 -0500

    :rocket: Working search!

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,48 @@
 const MiniSearch = require("minisearch")
 
-console.log("loading index")
-const miniSearch = new MiniSearch({
+let miniSearch = new MiniSearch({
     fields: ['title', 'content'],
-    storeFields: ['title', 'content']
+    storeFields: ['title', 'permalink']
+  })
+let allTitles = []
+
+fetch("/index.json")
+  .then(response => response.json())
+  .then(docs => {
+      allTitles = docs
+      miniSearch.addAll(allTitles)
   })
 
-const docs = fetch("/index.json")
-    .then(response => response.json())
-    .then(docs => {
-        miniSearch.addAll(docs)
-        // console.log(miniSearch.search("dumb"))
-        console.log(miniSearch.toJSON())
+
+  function formSubmit(event) {
+    let input = document.getElementById('site-search').value;
+    const titles = document.getElementsByClassName("post-title")
+    const root = titles[0].parentNode
+    while (titles.length > 0) {
+      titles[0].parentNode.removeChild(titles[0])
+    }
+
+    let results = miniSearch.search(input)
+    if (results == "") {
+      results = allTitles
+    }
+    else {
+      const title = document.createElement("p")
+      title.className = "post-title"
+      title.innerHTML = `<strong>Showing results for: ${input}</strong>`
+      root.appendChild(title)
+    }
+
+    results.map(d => {
+      const title = document.createElement("p")
+      title.className = "post-title"
+      title.innerHTML = `<a href="${d.permalink}">${d.title}</a>`
+      root.appendChild(title)
     })
+
+    event.preventDefault();
+  }
+  
+  const form = document.getElementById('search-form');
+  form.addEventListener('submit', formSubmit);
+  form.addEventListener('click', formSubmit)
