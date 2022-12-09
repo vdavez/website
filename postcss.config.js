4e0f540a08b3be3208aa3684a0ddbc47721621b5
V. David Zvenyach <dave@zvenyach.com>
2022-12-09 06:12:24 -0600

    :zap: Added CSSNano and PurgeCSS
    Reduced CSS size to 1/3 of the size!

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,23 @@
-// const purgecss = require('@fullhuman/postcss-purgecss')
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./hugo_stats.json'],
+  defaultExtractor: content => {
+    const els = JSON.parse(content).htmlElements;
+    return [
+      ...(els.tags || []),
+      ...(els.classes || []),
+      ...(els.ids || []),
+    ];
+  },
+  safelist: []
+});
 
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    // purgecss({
-      // content: ['./**/*.html'],
-      // defaultExtractor: content => content.match(/[\w-:/]+(?<!:)/g) || []
-    // })
+    require('cssnano')({
+      preset: 'default',
+    }),
+    purgecss
   ]
 }
