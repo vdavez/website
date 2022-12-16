14e09b1fed6aa4de213f3affaa3c03f06531c62c
V. David Zvenyach <dave@zvenyach.com>
2022-12-16 13:20:39 -0600

    :rocket: Working search!

--- assets/main.js
+++ assets/main.js
@@ -1,50 +1,81 @@
 const MiniSearch = require("minisearch")
 
-let miniSearch = new MiniSearch({
+const miniSearch = new MiniSearch({
     fields: ['title', 'content'],
-    storeFields: ['title', 'permalink', 'type']
+    storeFields: ['title', 'permalink']
   })
-let allTitles = []
 
 fetch("/index.json")
   .then(response => response.json())
   .then(docs => {
-      allTitles = docs
-      miniSearch.addAll(allTitles)
+      miniSearch.addAll(docs)
   })
 
-
-  function formSubmit(event) {
-    let input = document.getElementById('site-search').value;
-    const titles = document.getElementsByClassName("post-title")
-    const root = titles[0].parentNode
-    while (titles.length > 0) {
-      titles[0].parentNode.removeChild(titles[0])
-    }
-
-    let results = miniSearch.search(input)
-    if (results == "") {
-      results = allTitles.filter(d => {
-        return d.type == "posts"
-      })
-    }
-    else {
-      const title = document.createElement("p")
-      title.className = "post-title"
-      title.innerHTML = `<strong>Showing results for: ${input}</strong>`
-      root.appendChild(title)
-    }
-
-    results.map(d => {
-      const title = document.createElement("p")
-      title.className = "post-title"
-      title.innerHTML = `<a href="${d.permalink}">${d.title}</a>`
-      root.appendChild(title)
-    })
-
-    event.preventDefault();
+/**
+ * Removes all the children elements of the parent node
+ * Used to clear the select when searches change...
+ *
+ * @param {Element} parent the parent node
+ */
+const removeChildren = (parent) => {
+  while (parent.lastChild) {
+      parent.removeChild(parent.lastChild);
   }
+};
+
+/**
+ * 
+ * @param {event} event 
+ */
+const formChange = (event) => {
+  const input = document.getElementById('site-search').value;
+  const optionsList = document.getElementById("search-results-list-select")
+
+  removeChildren(optionsList)
+
+  // Hide the select element initially
+  optionsList.style.display = "none"
   
-  const form = document.getElementById('search-form');
-  form.addEventListener('submit', formSubmit);
-  form.addEventListener('click', formSubmit)
+  // Check to make sure that there's enough input text to bother with
+  // Could be better as a debounce...
+  if (input.length > 2) {
+
+      const results = miniSearch.search(input)
+      
+      if (results.length > 0) {
+        
+        // Add the options to the select
+        results.map(d => {
+          const optionElem = document.createElement("option")
+          optionElem.value = d.permalink
+          optionElem.text = d.title
+          optionsList.append(optionElem)
+        })
+
+        // Show the options with "display: block" but limit to 10 results 
+        optionsList.style.display = "block"
+        optionsList.size = (results.length < 10 ? results.length : 10)
+        optionsList.focus()
+      }
+    }
+  }
+  const input = document.getElementById('site-search');
+  input.addEventListener('input', formChange);
+
+  /**
+   * When you click on the option, go to the URL
+   */
+  const selectItem = (event) => {
+    if (event.key == "Enter" ||
+      event.key == " " ||
+      event.type == "click"
+      ) {
+        window.location.assign(`${optionsList.value}?q=${input.value}`);
+      }
+  }
+
+  const optionsList = document.getElementById("search-results-list-select")
+  optionsList.style.display = "none"
+  
+  optionsList.addEventListener('click',selectItem);
+  optionsList.addEventListener('keydown',selectItem)
